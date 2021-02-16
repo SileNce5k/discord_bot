ba6e0069e22cafb4d2745932e715e7328761ccc0
SileNce5k <ozzynexus@gmail.com>
2021-02-16 21:23:24 +0100

    fuck I don't remember what I did. All I know is that it's not important. At least I fucking hope so lol

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -35,7 +35,7 @@ module.exports = {
             .setTitle("Information about bot")
             .setTimestamp()
             .setFooter('https://github.com/SileNce5k/Soilens_BotJS')
-            .setAuthor("soilens bot", "https://cdn.discordapp.com/avatars/481128222147477506/1a30f57f8e403f54aaca502012aeff14.png?size=2048")
+            .setAuthor(client.user.username, "https://cdn.discordapp.com/avatars/481128222147477506/1a30f57f8e403f54aaca502012aeff14.png?size=2048")
             .addFields(
                 { name: "General info", value: `Name: ${client.user.username}\nPrefix: ${prefix}\nCreation Date: ${creationDate.getFullYear()}-${monthcreate}-${datecreate} ${hour}:${minutes}:${seconds}` },
             )
