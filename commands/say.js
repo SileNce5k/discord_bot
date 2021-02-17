310d737201468caed83c52d798f70beb605edca9
SileNce5k <ozzynexus@gmail.com>
2021-02-17 03:19:53 +0100

    'say' command now checks length of args so it doesn't attempt to send an empty message.

--- commands/say.js
+++ commands/say.js
@@ -2,6 +2,11 @@ module.exports = {
 	name: 'say',
 	description: 'Repeats arguments',
 	execute(message, args) {
+
+		if(args.length == 0){
+		message.channel.send("Can't send empty message");
+		}else
 		message.channel.send(args.join(" "))
+		
 	}
 };
