7b7054e9f563342084606fb42446e7f26a9b3bb9
SileNce5k <ozzynexus@gmail.com>
2021-03-08 20:35:50 +0100

    Remove console.log debugging

--- util/reloadCommands.js
+++ util/reloadCommands.js
@@ -7,15 +7,10 @@ module.exports = function (client) { //TODO: Add ability to reload specified com
     let commandFiles = fs.readdirSync(filepath).filter(file => file.endsWith('.js'));
     if (client.commands.size != 0) {
         for (const i of commandFiles) {
-            console.log(i)
             delete require.cache[require.resolve(`../${filepath}${i}`)];
         }
     }
-
-    console.log(client.commands.size)
     client.commands.clear()
-    console.log("test")
-
     for (const file of commandFiles) {
         const command = require(`../commands/${file}`);
         client.commands.set(command.name, command);
