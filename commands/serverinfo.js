fb780bda1be088e12d698dc29db1fd1f9ee9a707
SileNce5k <ozzynexus@gmail.com>
2021-03-08 21:28:31 +0100

    Add more info to serverinfo

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -4,14 +4,17 @@ module.exports = {
 	name: 'serverinfo',
 	description: 'Displays information about the server',
 	execute(message) {
+        console.log(message.guild.emojis.cache)
         const embed = new Discord.MessageEmbed()
+        
             .setThumbnail(message.guild.iconURL())
             .setColor("#ee7939")
             .setTimestamp()
-            .addField("Server Owner: ", message.guild.owner)
+            .addField("Server Owner: ", `<@${message.guild.ownerID}>`)
             .addField("Server Name: ", message.guild.name)
-            .addField("Created", message.guild.createdAt.toISOString())
-
+            .addField("Created", message.guild.createdAt.toUTCString())
+            .addField("Members: ", message.guild.memberCount)
+            //.addField("Emojis: ", message.guild.emojis.cache.array().length)
 
         message.channel.send(embed);
     
