86b3e57b4ab4adc50df40c571962aabdbcc71b39
SileNce5k <ozzynexus@gmail.com>
2021-03-10 23:48:41 +0100

    Delete cache of required files in util/
    Addresses #5.

--- util/reloadCommands.js
+++ util/reloadCommands.js
@@ -1,18 +1,22 @@
 const fs = require('fs')
-const filepath = 'commands/'
+const commandPath = 'commands/'
+const utilPath = 'util/'
 
 
 module.exports = function (client) {
-
-    let commandFiles = fs.readdirSync(filepath).filter(file => file.endsWith('.js'));
+    let utilFiles = fs.readdirSync(utilPath).filter(file => file.endsWith('.js'));
+    let commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
     if (client.commands.size != 0) {
         for (const i of commandFiles) {
-            delete require.cache[require.resolve(`../${filepath}${i}`)];
+            delete require.cache[require.resolve(`../${commandPath}${i}`)];
+        }
+        for (const i of utilFiles){
+            delete require.cache[require.resolve(`../${utilPath}${i}`)]
         }
     }
     client.commands.clear()
     for (const file of commandFiles) {
-        const command = require(`../commands/${file}`);
+        const command = require(`../${commandPath}${file}`);
         client.commands.set(command.name, command);
     }
 }
