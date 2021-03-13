5b98166a2ba1648839bb41e39b1d99cb46cd8e46
SileNce5k <ozzynexus@gmail.com>
2021-03-13 04:30:16 +0100

    Add check for permissions on prefix command
    Now only people with the MANAGE_GUILD permission can edit the prefix of
    that specific server.

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,9 +1,14 @@
+const parseMention = require("../util/parseMention");
 const setServerPrefix = require("../util/setServerPrefix");
 
 module.exports = {
     name: 'prefix',
     description: 'Change the prefix of the bot in this server.',
     execute({ message, client, args, prefix }) {
+        if (!message.member.hasPermission('MANAGE_GUILD')) {
+            message.channel.send("You do not have sufficient permissions(MANAGE_GUILD) to change the prefix of this server")
+            return;
+        }
         if (!args[0]) {
             message.channel.send(`To change the prefix, execute \`${prefix}prefix <newPrefix>\``);
             return;
