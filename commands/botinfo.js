88100600b914576161ee78010fb167d69d369e5b
SileNce5k <ozzynexus@gmail.com>
2021-03-15 16:53:50 +0100

    Update github link in botinfo

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -15,7 +15,7 @@ module.exports = {
             .setTimestamp()
             .setAuthor(client.user.username, "https://cdn.discordapp.com/avatars/481128222147477506/1a30f57f8e403f54aaca502012aeff14.png?size=2048")
             .addFields({ 
-                name: "General info", value: `Name: ${client.user.username}\nPrefix: ${prefix}\nTotal Servers: ${client.guilds.toString().length}\nCreation Date: ${createJoin.creation}\nSource: [Click Here](https://github.com/SileNce5k/Soilens_BotJS)`,
+                name: "General info", value: `Name: ${client.user.username}\nPrefix: ${prefix}\nTotal Servers: ${client.guilds.toString().length}\nCreation Date: ${createJoin.creation}\nSource: [Click Here](https://github.com/SileNce5k/discord_bot)`,
             },)
 
         message.channel.send(embed)
