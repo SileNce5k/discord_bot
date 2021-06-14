86d97622a8b50ddc856a6b86ba66db25ce970119
SileNce5k <ozzynexus@gmail.com>
2021-06-14 20:24:27 +0200

    Indent code after if statement

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ client.once('ready', () => {
 	console.log('Ready!');
 	client.user.setActivity(globalPrefix, { type: 'LISTENING' });
 	if (enableLoginMessage === true)
-	try{
-	client.channels.cache.get(loginChannel).send(loginMessage)
-	}catch(err){
-		console.log("Failed trying to send a message on login.\n")
-	}
+		try{
+		client.channels.cache.get(loginChannel).send(loginMessage)
+		}catch(err){
+			console.log("Failed trying to send a message on login.\n")
+		}
 	loadServerPrefixes(client)
 });
 
