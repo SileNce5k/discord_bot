4c1a87118be1d7c243b357dbb2518d99069f170e
SileNce5k <ozzynexus@gmail.com>
2021-06-19 12:55:19 +0200

    Add message.js
    Forgot to commit this with 4630cefd47271929fd288914917eb0fd3c3193c9 like
    a fucking idiot.

---
+++ server/message.js
@@ -0,0 +1,30 @@
+module.exports = function(client, owners, message, globalPrefix){
+
+	if (client.serverPrefixes.get(message.guild.id)) {
+		prefix = client.serverPrefixes.get(message.guild.id)
+	} else
+		prefix = globalPrefix
+
+	let args = message.content.slice(prefix.length).split(" ")
+
+	const commandName = args.shift().toLowerCase();
+	const command = client.commands.get(commandName);
+	const netModule = client.netmodules.get(commandName);
+	if (!message.guild || message.author.bot || !message.content.startsWith(prefix)) return;
+	if (!command){
+		if (netModule){
+			try {
+				netModule.execute({message: message, args: args, client: client, prefix: prefix})
+			}catch(e){
+				console.log(e)
+			}
+		}
+		return;
+	}
+	if (command.admin && owners.indexOf(message.author.id.toString()) == -1) return;
+	try {
+		command.execute({ message: message, args: args, client: client, prefix: prefix, owners: owners})
+	} catch (error) {
+		console.log(`${error}\n-------`)
+	}
+}
