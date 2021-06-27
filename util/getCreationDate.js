8b92667315c91822dff179d30cf6cb83e2d4ba34
SileNce5k <ozzynexus@gmail.com>
2021-06-27 20:14:46 +0200

    Add leading zeros and add one to month
    Forgot about the fucking padding and month not being correct because
    fuck javascript. Hopefully I didn't forget about anything else.

--- util/getCreationDate.js
+++ util/getCreationDate.js
@@ -1,4 +1,4 @@
 module.exports = function(user){
 	let date = user.createdAt;
-	return date.getUTCFullYear().toString()+"-"+date.getUTCMonth()+"-"+date.getUTCDate()+" "+date.getUTCHours()+":"+date.getUTCMinutes()+":"+date.getUTCSeconds();
+	return date.getUTCFullYear() + '-' + ('0' + (date.getUTCMonth()+1)).slice(-2) + '-' + ('0' + date.getUTCDate()).slice(-2)+" "+date.getUTCHours()+":"+date.getUTCMinutes()+":"+date.getUTCSeconds();
 }
