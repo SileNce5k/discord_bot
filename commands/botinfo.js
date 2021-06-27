2b3e6cd5985e787ed274ed1c4578aa6bf3f59aef
SileNce5k <ozzynexus@gmail.com>
2021-06-27 02:44:30 +0200

    Don't hardcode client avatar

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -13,7 +13,7 @@ module.exports = {
 			.setColor(15780145)
 			.setTitle("Information about bot")
 			.setTimestamp()
-			.setAuthor(client.user.username, "https://cdn.discordapp.com/avatars/481128222147477506/1a30f57f8e403f54aaca502012aeff14.png?size=2048")
+			.setAuthor(client.user.username, client.user.avatarURL({ dynamic: true, size: 4096 }))
 			.addFields({ 
 				name: "General info", value: `Name: ${client.user.username}\nPrefix: ${prefix}\nTotal Servers: ${client.guilds.toString().length}\nCreation Date: ${createJoin.creation}\nSource: [Click Here](https://github.com/SileNce5k/discord_bot)`,
 			},)
