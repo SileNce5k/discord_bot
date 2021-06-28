a1dc9d1bc7a1535c937e38fa87806eea84996604
SileNce5k <ozzynexus@gmail.com>
2021-06-28 13:35:06 +0200

    Fix getCreationDate
    Forgot I had to do user.user.createdAt instead of just user.createdAt.

--- util/getCreationDate.js
+++ util/getCreationDate.js
@@ -1,4 +1,4 @@
 module.exports = function(user){
-	let date = user.createdAt;
+	let date = user.user.createdAt;
 	return date.getUTCFullYear() + '-' + ('0' + (date.getUTCMonth()+1)).slice(-2) + '-' + ('0' + date.getUTCDate()).slice(-2)+" "+date.getUTCHours()+":"+date.getUTCMinutes()+":"+date.getUTCSeconds();
 }
