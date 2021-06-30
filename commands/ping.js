177d5ca6a4c0350b68a4f00ffadf15cdd1ee54cd
SileNce5k <ozzynexus@gmail.com>
2021-06-30 19:31:34 +0200

    Small edit

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ping', // Keep it to one word
 	description: 'Just ping.',
 	execute({message, client}) { //parameters you can use for netload: message, args, client, prefix 
-		message.channel.send(`Ping is ${client.ws.ping}ms`)
+		message.channel.send(`Pong.\n${client.ws.ping}ms`)
 	}
 };
