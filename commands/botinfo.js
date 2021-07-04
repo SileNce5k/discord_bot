edba188072d0393c50631cbc70c4ba8f8fae510e
SileNce5k <ozzynexus@gmail.com>
2021-07-04 20:41:55 +0200

    Show correct amount of servers on botinfo command

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -7,13 +7,18 @@ module.exports = {
 	name: 'botinfo',
 	description: 'Shows information about the bot',
 	execute({message, client, prefix}) {
+		let guildCount = 0;
+		client.guilds.cache.each(guild => {
+			guildCount++
+		});
+		
 		const embed = new Discord.MessageEmbed()
 			.setColor(15780145)
 			.setTitle("Information about bot")
 			.setTimestamp()
 			.setAuthor(client.user.username, client.user.avatarURL({ dynamic: true, size: 4096 }))
 			.addFields({ 
-				name: "General info", value: `Name: ${client.user.username}\nPrefix: ${prefix}\nTotal Servers: ${client.guilds.toString().length}\nCreation Date: ${getCreationDate(client.user)}\nSource: [Click Here](https://github.com/SileNce5k/discord_bot)`,
+				name: "General info", value: `Name: ${client.user.username}\nPrefix: ${prefix}\nTotal Servers: ${guildCount}\nCreation Date: ${getCreationDate(client)}\nSource: [Click Here](https://github.com/SileNce5k/discord_bot)`,
 			},)
 
 		message.channel.send(embed)
