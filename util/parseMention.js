453bf35803911044e914d53c5c0a227c637a815c
SileNce5k <ozzynexus@gmail.com>
2021-07-04 13:17:45 +0200

    Make code a tiny bit cleaner

--- util/parseMention.js
+++ util/parseMention.js
@@ -1,10 +1,7 @@
 module.exports = function (text, guild) {
 	let id = "";
 	let isMention = false;
-	if (
-		text.substring(0, 2) == "<@" &&
-		text.substring(text.length - 1, text.length) == ">"
-	) {
+	if (text.substring(0, 2) == "<@" && text.substring(text.length - 1, text.length) == ">") {
 		let start = 2;
 		if (text.substring(0, 3) == "<@!") start = 3;
 		id = text.substring(start, text.length - 1);
