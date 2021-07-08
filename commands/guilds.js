a2920bc381eef5ced913317b5cb2632980db2529
SileNce5k <ozzynexus@gmail.com>
2021-07-08 20:08:36 +0200

    Add guilds command that sends all guild names

---
+++ commands/guilds.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'guilds',
+	description: 'Returns guild names',
+	execute({message, client}) { 
+		let guildNames = "";
+		client.guilds.cache.each(guild => {
+			guildNames = guildNames + guild.name + "\n";
+		});
+		message.channel.send(guildNames)
+	}
+};
