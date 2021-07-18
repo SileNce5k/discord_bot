14f2ee8f7a87bca0a0e370b65dff497be63b2002
SileNce5k <ozzynexus@gmail.com>
2021-07-18 03:57:13 +0200

    Remove some console.log debugging

--- util/reloadCommands.js
+++ util/reloadCommands.js
@@ -6,22 +6,15 @@ const utilPath = 'util/'
 module.exports = function (client) {
 	let utilFiles = fs.readdirSync(utilPath).filter(file => file.endsWith('.js'));
 	let subdir = fs.readdirSync(commandPath)//.filter(file => file.endsWith('.js'));
-	console.log("Temp 1 = \n"+subdir + "\nEnd of Temp 1")
 	let commandFiles = [];
 	subdir.forEach(item => {
-		
 		if(fs.statSync(commandPath+item).isDirectory()){
-			
 			let subdirFiles = fs.readdirSync(commandPath+item).filter(file => file.endsWith('.js'))
-			
 			subdirFiles.forEach(file => {
-				console.log(commandPath+item+"/"+file)
 				commandFiles.push(commandPath+item+"/"+file)
-				
 			});
 		}
 	});
-	console.log(commandFiles)
 	if (client.commands.size != 0) {
 		for (const i of commandFiles) {
 			delete require.cache[require.resolve(`../${commandPath}${i}`)];
@@ -35,6 +28,5 @@ module.exports = function (client) {
 		const command = require(`../${file}`);
 		client.commands.set(command.name, command);
 	}
-	console.log(client.commands)
 }
 
