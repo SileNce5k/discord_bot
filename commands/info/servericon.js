32dbcd062e0e17db5a873b228d4a4a6f0a97ebbd
SileNce5k <ozzynexus@gmail.com>
2021-07-22 02:32:34 +0200

    Add servericon command

---
+++ commands/info/servericon.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name = "servericon",
+	description = "Send the server icon",
+	execute({message, client}){
+		message.channel.send(client.user.avatarURL({ dynamic: true, size: 4096 }))
+	}
+}
