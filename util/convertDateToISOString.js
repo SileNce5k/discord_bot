41734e7412c1e85da96e0b4d48ecaea6ae914b61
SileNce5k <ozzynexus@gmail.com>
2021-07-24 19:44:12 +0200

    Remove whitespace

--- util/convertDateToISOString.js
+++ util/convertDateToISOString.js
@@ -1,6 +1,3 @@
 module.exports = function (date){
-	
-
-
 	return date.getUTCFullYear() + '-' + ('0' + (date.getUTCMonth()+1)).slice(-2) + '-' + ('0' + date.getUTCDate()).slice(-2)+" "+('0' + date.getUTCHours()).slice(-2)+":"+('0' + date.getUTCMinutes()).slice(-2)+":"+('0'+date.getUTCSeconds()).slice(-2);
-} 
+}
