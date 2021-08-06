fd2ebd25f42863a49ddfdf706efd4b7e203d99e1
SileNce5k <ozzynexus@gmail.com>
2021-08-06 19:07:02 +0200

    Set admin to true on send command

--- commands/admin/send.js
+++ commands/admin/send.js
@@ -1,6 +1,7 @@
 module.exports = {
 	name: 'send',
 	description: 'Send a message to a specific channel',
+	admin: true,
 	execute({client, args}){
 		let channel = args[0];
 		let message = args.slice(1, args.length)
