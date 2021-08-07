2ac01e2ce10d8daca6aa8f6d001f99d3ae264a08
SileNce5k <ozzynexus@gmail.com>
2021-08-07 23:50:58 +0200

    Refactor randomNumber function

--- util/randomNumber.js
+++ util/randomNumber.js
@@ -1,3 +1,1 @@
-module.exports = function (min, max) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
-}
+module.exports = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
