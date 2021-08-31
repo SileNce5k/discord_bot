607ae7d8533d7bb653d00307af6d33d09560b88c
SileNce5k <ozzynexus@gmail.com>
2021-08-31 18:20:25 +0200

    Use a for loop instead of being an idiot

--- util/setPresence.js
+++ util/setPresence.js
@@ -3,9 +3,10 @@ const getGuildCount = require("./getGuildCount")
 module.exports = function ({presenceText, presenceType, client}) {
 	const {globalPrefix} = require ('../data/config.json')
 	let guildCount = getGuildCount(client)
-	let regex = /<guilds>/g
-	presenceText = presenceText.replace(regex, guildCount)
-	regex = /<prefix>/g
-	presenceText = presenceText.replace(regex, globalPrefix)
+	let regex = [/<guilds>/g,/<prefix>/g];
+	let replaceValue = [guildCount, globalPrefix];
+	for(let i = 0; i < regex.length; i++){
+		presenceText = presenceText.replace(regex[i], replaceValue[i]);
+	}
 	client.user.setActivity(presenceText, { type: presenceType });
 }
