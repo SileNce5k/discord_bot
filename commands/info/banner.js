bc3b1822362875f8317f763acd17205eb7e700cd
SileNce5k <ozzynexus@gmail.com>
2022-01-22 11:18:17 +0100

    Only execute bannerURL function once

--- commands/info/banner.js
+++ commands/info/banner.js
@@ -5,8 +5,10 @@ module.exports = {
 	description: 'Returns server banner',
 	execute({message}) {
 
-		if(message.guild.bannerURL())
-			message.channel.send(message.guild.bannerURL({format: 'png', size: 4096}))
+		let bannerURL = message.guild.bannerURL({format: 'png', size: 4096});
+		console.log(bannerURL)
+		if(bannerURL)
+			message.channel.send(bannerURL)
 		else
 			message.channel.send("There is no banner.")
 	}
