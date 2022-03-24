4cba23dbe297c3a89ae523dd5869ce27e22226a9
SileNce5k <ozzynexus@gmail.com>
2022-03-24 23:33:30 +0100

    Revert "If no moreHelp is available..."
    This reverts commit bcff645ba7cd4738e12245fefcdac41dac9802eb.

--- commands/info/help.js
+++ commands/info/help.js
@@ -50,29 +50,22 @@ module.exports = {
 					commands = commands + `${element}\n`
 				});
 				} else {
-					//commands = command.description;
 					noHelp = 1;
 				}
 				break;
 			}
 		}
 		let regex = /<prefix>/g
+		if(commands === ""){
+			noHelp = 1;
+		}
 		commands = commands.replace(regex, prefix)
 		embed.addFields(
 			{ name: " ", value: commands },
 		)
-		if(commands === false){
-			message.channel.send("There is no command with that name");
-		}
-		else if(noHelp == 0){
+		if(noHelp == 0)
 			message.channel.send({embeds :[embed]});
-		}
-		else {
-			/*embed.addFields(
-				{name: "Help", value: commands}
-			)*/
-			message.channel.send({embeds :[embed]})
-		}
-		
+		else
+			message.channel.send("Either there is no command with that name, or there is no specific help for it.")
 	},
 };
