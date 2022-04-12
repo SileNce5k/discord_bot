2d8ca95ecb2d6d2024bee2964589d52f0a20cb44
SileNce5k <ozzynexus@gmail.com>
2022-04-12 21:47:46 +0200

    Fix message empty crashing bot

--- commands/misc/ask.js
+++ commands/misc/ask.js
@@ -4,11 +4,13 @@ module.exports = {
 	name: 'ask',
 	description: 'Randomly answer with Yes, or No, or if you ask a question with "or" in it, answer with either statement before or after "or"',
 	execute({message, args}) {
-		let answer = randomNumber(0, 1) === 1 ? "Yes" : "No";
+		let answer = "";
 		if(args.length > 2 && args.includes("or")){
 			let question = args.join(" ").split("or ");
 			answer = question[randomNumber(0, question.length -1)];
 		}
+		if(answer === "")
+			answer = randomNumber(0, 1) === 1 ? "Yes" : "No";
 		message.channel.send(answer)
 	}
 };
