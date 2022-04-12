0181007ad501de96dcadaa0a54a05dcc4892d583
SileNce5k <ozzynexus@gmail.com>
2022-04-12 21:02:51 +0200

    If args contain "or" return either side randomly

--- commands/misc/ask.js
+++ commands/misc/ask.js
@@ -2,9 +2,13 @@ const randomNumber = require("../../util/randomNumber");
 
 module.exports = {
 	name: 'ask',
-	description: 'Randomly answer with Yes, or No',
-	execute({message}) {
+	description: 'Randomly answer with Yes, or No, or if you ask a question with "or" in it, answer with either statement before or after "or"',
+	execute({message, args}) {
 		let answer = randomNumber(0, 1) === 1 ? "Yes" : "No";
+		if(args.length > 2 && args.includes("or")){
+			let question = args.join(" ").split("or ");
+			answer = randomNumber(0, 1) === 1 ? question[0] : question[1];
+		}
 		message.channel.send(answer)
 	}
 };
