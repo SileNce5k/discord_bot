517f13291fb72a2547d91d7f9699a0c99990e504
SileNce5k <ozzynexus@gmail.com>
2022-05-15 00:18:06 +0200

    Add timer command

---
+++ commands/misc/timer.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: "timer",
+	description: "Set a timer for a time in minutes. Bot will mention you and the custom message after time has passed.",
+	execute({message, args}) {
+		if(args.length < 2)
+			return message.channel.send("Please specify a time in minutes, and a message to send after the timer has finished");
+		let time = args[0] * 60000;
+		let sendText = args.slice(1).join(" ");
+		let timer = setTimeout(function(){
+			message.channel.send(`<@${message.author.id}>, ${sendText}`);
+		}, time);
+		message.channel.send(`I will remind you in ${time / 60000} seconds`);
+	}
+};
