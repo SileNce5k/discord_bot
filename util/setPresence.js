92787eb2d1888a6452ee92d2da4464f49fdff0d2
SileNce5k <ozzynexus@gmail.com>
2022-06-03 15:43:32 +0200

    Fix plural bug + change ternaries to ifs

--- util/setPresence.js
+++ util/setPresence.js
@@ -4,8 +4,19 @@ module.exports = function ({presenceText, presenceType, client}) {
 	const {globalPrefix} = require ('../data/config.json')
 	let guildCount = getGuildCount(client)
 	let uptime = parseMS(client.uptime);
-	let uptimeSingularOrPlural = uptime.hours > 1 || uptime.minutes > 1 ? "s" : "";
-	let uptimeFormat = uptime.hours >= 1 || uptime.days >= 1 ? uptime.days * 24 + uptime.hours + ` hour${uptimeSingularOrPlural}`: uptime.minutes + ` minute${uptimeSingularOrPlural}`;
+
+	let uptimeFormat = "";
+	let uptimeSingularOrPlural;
+	if(uptime.hours >= 1 || uptime.days >= 1){
+		uptimeSingularOrPlural = uptime.hours > 1 ? "s" : "";
+		uptimeFormat = uptime.days * 24 + uptime.hours + ` hour${uptimeSingularOrPlural}`
+	}else if(uptime.minutes > 1){
+		uptimeSingularOrPlural = uptime.minutes > 1 ? "s" : "";
+		uptimeFormat = uptime.minutes + ` minute${uptimeSingularOrPlural}`;
+	}else{
+		uptimeFormat = `less than a minute`
+	}
+
 	let regex = [/\${guilds}/g,/\${prefix}/g,/\${uptime}/g];
 	let replaceValue = [guildCount, globalPrefix, uptimeFormat];
 	for(let i = 0; i < regex.length; i++){
