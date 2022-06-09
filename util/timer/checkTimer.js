6bbc8efcdd881d345abd3e4dd2553dcd5a8576dc
SileNce5k <ozzynexus@gmail.com>
2022-06-09 19:23:45 +0200

    Fix comparison

--- util/timer/checkTimer.js
+++ util/timer/checkTimer.js
@@ -2,7 +2,7 @@ const sendTimerReminder = require('./sendTimerReminder')
 module.exports = function (client) {
 	const checkTimer = require('./checkTimer')
 	client.timers.forEach(timer => {
-		if(parseInt(timer.reminderDate) >= Math.floor(new Date() / 1000)){
+		if(parseInt(timer.reminderDate) <= Math.floor(new Date() / 1000)){
 			sendTimerReminder(client, timer);
 			client.timers.pop(timer);
 		}
