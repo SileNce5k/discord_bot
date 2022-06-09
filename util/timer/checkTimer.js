5bb6acc9e47ead77d13b6f7588ce93692c87a7b0
SileNce5k <ozzynexus@gmail.com>
2022-06-09 19:25:58 +0200

    Write to timers.json after reminder has passed

--- util/timer/checkTimer.js
+++ util/timer/checkTimer.js
@@ -1,10 +1,12 @@
 const sendTimerReminder = require('./sendTimerReminder')
+const fs = require('fs')
 module.exports = function (client) {
 	const checkTimer = require('./checkTimer')
 	client.timers.forEach(timer => {
 		if(parseInt(timer.reminderDate) <= Math.floor(new Date() / 1000)){
 			sendTimerReminder(client, timer);
 			client.timers.pop(timer);
+			fs.writeFileSync('data/timers.json', JSON.stringify(client.timers, null, 4))
 		}
 	});
 	setTimeout(checkTimer, 1000, client);
