bdc08b3153284abf9789299daafecaa78e6cb1f9
SileNce5k <ozzynexus@gmail.com>
2024-03-03 19:07:45 +0100

    Improve the timer reminder message

--- util/timer/sendTimerReminder.js
+++ util/timer/sendTimerReminder.js
@@ -1,3 +1,3 @@
 module.exports = function (client, timer) {
-	client.channels.cache.get(timer.channel).send(`<@${timer.user}>, ${timer.customMessage}`);
+	client.channels.cache.get(timer.channel).send(`<@${timer.user}>, your timer (ID: ${timer.ID}) is up!\nCustom message:\n"${timer.customMessage}"`);
 }
