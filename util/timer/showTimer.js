6e8faadd97e7e0b646c32909195d981faf0ce619
SileNce5k <ozzynexus@gmail.com>
2024-04-10 13:47:50 +0200

    Specify if the timer doesn't belong to author

--- util/timer/showTimer.js
+++ util/timer/showTimer.js
@@ -4,14 +4,17 @@ module.exports = async function (authorID, timerID) {
     const db = new sqlite3.Database(databasePath);
     let sendText = "";
     await new Promise((resolve, reject) => {
-        db.get(`SELECT * FROM timers WHERE ID = ? AND user = ?`, [timerID, authorID],
+        db.get(`SELECT * FROM timers WHERE ID = ?`, [timerID],
         function (error, timer){
             if(error){
                 sendText = "An error occured while trying to read timer from database. Check console.";
                 console.error("Error while trying to read timer from database: ", error)
                 reject(error);
             }else{
-                if(timer === undefined){
+                if(authorID !== timer.user){
+                    sendText = "This timer does not belong to you."
+                }
+                else if(timer === undefined){
                     sendText = "Timer not found";
                 }else{
                     sendText = `${timer.ID} will remind you <t:${timer.reminderTime.toFixed(0)}:R> (<t:${timer.reminderTime.toFixed(0)}:f>), with the message being:\n${timer.customMessage}`;
