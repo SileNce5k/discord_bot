797c2b0118ee67b737b41cd080603a42e82d150e
SileNce5k <ozzynexus@gmail.com>
2024-06-11 18:10:50 +0200

    Add banner command for users

---
+++ commands/info/banner.js
@@ -0,0 +1,25 @@
+const parseMention = require('../../util/parseMention');
+module.exports = {
+    name: 'banner',
+    description: 'Shows the banners of a user',
+    async execute({message, args, client}) {
+		let info;
+		if (!args[0]) {
+			info = message.author.id;
+		} else {
+			info = parseMention(args[0], message.guild);
+		}
+        if(info === ""){
+            message.channel.send("Invalid user");
+            return;
+        }
+		let forceFetchedUser = await client.users.fetch(info, {force: true});
+        console.log(forceFetchedUser)
+        let banner = forceFetchedUser.bannerURL({dynamic: true, size: 4096});
+        if(banner == null){
+            message.channel.send("User does not have a banner");
+        } else {
+            message.channel.send(banner);
+        }
+    }
+};
