4ca13712ed00ed3c3bc9dc8464b1e83ddf347cc7
SileNce5k <ozzynexus@gmail.com>
2024-10-22 11:52:29 +0200

    Add test for calculateReloaded

---
+++ tests/calculateReloaded.test.js
@@ -0,0 +1,26 @@
+const calculateReloaded = require('../util/calculateReloaded')
+
+let client = {commands: new Map()};
+for(let i = 0; i < 10; i++){
+    client.commands.set(i, i * 420);
+}
+let beforeSizes = [0, 12, 10, 9, 11, 6, 104];
+let expectedResults = [
+    `10 modules were added, and a total of 10 were reloaded.`,
+    `10 modules were reloaded after 2 were disabled.`,
+    `10 modules were reloaded.`,
+    `1 module was added, and a total of 10 were reloaded.`,
+    `10 modules were reloaded after 1 module was deleted.`,
+    `4 modules were added, and a total of 10 were reloaded.`,
+    `10 modules were reloaded after 94 were disabled.`
+]
+
+if(beforeSizes.length != expectedResults.length) throw "Error: beforeSizes and expectedResults are unequal length"
+
+for(let i = 0; i < beforeSizes.length; i++){
+    test('calculateReloaded test', () => {
+        expect(calculateReloaded(beforeSizes[i], client)).toBe(expectedResults[i])
+        
+        });
+
+}
