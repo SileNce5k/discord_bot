ee6debb7152e1bcd0e6e66a799c34da823d5613d
SileNce5k <ozzynexus@gmail.com>
2024-10-25 22:10:03 +0200

    Add test for getGuildCount

---
+++ tests/getGuildCount.test.js
@@ -0,0 +1,17 @@
+const getGuildCount = require('../util/getGuildCount');
+
+
+
+test("Testing getGuildCount", () => {
+    for(let i = 1; i < 200000; i = i+i*30 ){
+        let client = {guilds: {cache: new Map()}}
+        client.guilds.cache.each = client.guilds.cache.forEach;
+
+        for(let j = 0; j < i; j++){
+            client.guilds.cache.set(`num: ${j}`, j);
+        }
+        
+        expect(getGuildCount(client)).toBe(i);
+        
+    }
+})
