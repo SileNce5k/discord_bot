f28055e02405ed2f329f3aff407fda2bf1134968
SileNce5k <ozzynexus@gmail.com>
2024-11-06 04:06:34 +0100

    Refactor server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const createInitialConfig = require("./util/createInitialConfig")
 const convertTimerJSONToSQL = require('./util/timer/convertTimerJSONToSQL.js');
+const createTimersTable = require('./server/createDatabaseTables/createTimersTable');
 if(!fs.existsSync("./data/config.json")) {
 	createInitialConfig();
 }
@@ -8,7 +9,6 @@ async function checkAndConvertJSONToSQL(){
 	process.stdout.write("Checking if timers.json exists... ")
 	if(fs.existsSync("./data/timers.json")){
 		process.stdout.write(true + "\n")
-		await createTimerTables();
 		await convertTimerJSONToSQL();
 		fs.renameSync('data/timers.json', 'data/timers.json.old');
 		console.log("Renamed timers.json to timers.json.old");
@@ -16,57 +16,70 @@ async function checkAndConvertJSONToSQL(){
 		process.stdout.write(false + "\n")
 	}
 }
-const createTimerTables = require('./server/createDatabaseTables/createTimersTable');
-const createLastfmTable = require('./server/createDatabaseTables/createLastfmTable');
-createLastfmTable();
-checkAndConvertJSONToSQL();
-const { Collection, Client, GatewayIntentBits, Partials } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, 
-									  GatewayIntentBits.GuildMessages, 
-									  GatewayIntentBits.MessageContent, 
-									  GatewayIntentBits.GuildMembers, 
-									  GatewayIntentBits.GuildPresences
-									], partials: [Partials.Channel] });
-const {
-	globalPrefix,
-	token,
-	loginMessage,
-	loginChannel,
-	enableLoginMessage,
-	owners,
-	presenceType,
-	presenceText
-} = require('./data/config.json');
-
-client.settings = new Collection();
-client.commands = new Collection();
-client.serverPrefixes = new Collection();
 
 
-client.settings.set("presenceType", presenceType);
-client.settings.set("presenceText", presenceText);
 
-const reloadCommands = require("./util/reloadCommands.js");
-const onMessage = require('./server/message');
-const onReady = require('./server/ready');
+function startBot(){
+	const { Collection, Client, GatewayIntentBits, Partials } = require('discord.js');
+	const client = new Client({ intents: [GatewayIntentBits.Guilds, 
+										GatewayIntentBits.GuildMessages, 
+										GatewayIntentBits.MessageContent, 
+										GatewayIntentBits.GuildMembers, 
+										GatewayIntentBits.GuildPresences
+										], partials: [Partials.Channel] });
+	const {
+		globalPrefix,
+		token,
+		loginMessage,
+		loginChannel,
+		enableLoginMessage,
+		owners,
+		presenceType,
+		presenceText
+	} = require('./data/config.json');
 
-reloadCommands(client)
+	client.settings = new Collection();
+	client.commands = new Collection();
+	client.serverPrefixes = new Collection();
 
-client.once('ready', () => {
-	onReady(client, enableLoginMessage, loginChannel, loginMessage)
+
+	client.settings.set("presenceType", presenceType);
+	client.settings.set("presenceText", presenceText);
+
+	const reloadCommands = require("./util/reloadCommands.js");
+	const onMessage = require('./server/message');
+	const onReady = require('./server/ready');
+
+	reloadCommands(client)
+
+	client.once('ready', () => {
+		onReady(client, enableLoginMessage, loginChannel, loginMessage)
+	});
+
+	client.once('reconnecting', () => {
+		console.log('Reconnecting!');
+	});
+
+	client.once('disconnect', () => {
+		console.log('Disconnect!');
+	});
+
+	client.on('messageCreate', async message => {
+		onMessage(client, owners, message, globalPrefix);
+	});
+
+
+	client.login(token);
+	
+}
+
+async function prepareBot(){
+	const createLastfmTable = require('./server/createDatabaseTables/createLastfmTable');
+	await createTimersTable();
+	await createLastfmTable();
+	await checkAndConvertJSONToSQL();
+}
+
+prepareBot().then( () => {
+	startBot();
 });
-
-client.once('reconnecting', () => {
-	console.log('Reconnecting!');
-});
-
-client.once('disconnect', () => {
-	console.log('Disconnect!');
-});
-
-client.on('messageCreate', async message => {
-	onMessage(client, owners, message, globalPrefix);
-});
-
-
-client.login(token);
