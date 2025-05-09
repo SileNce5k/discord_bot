2f371d00ed31b73942257f93ae7a81d07271c2c1
SileNce5k <ozzynexus@gmail.com>
2025-05-09 04:03:32 +0200

    Add message latency to ping command

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -1,7 +1,11 @@
 module.exports = {
 	name: 'ping',
 	description: 'Just ping.',
-	execute({message, client}) {
-		message.channel.send(`Pong.\n${client.ws.ping}ms`)
+	async execute({message, client}) {
+		let apiLatency = client.ws.ping;
+		const time = process.hrtime.bigint();
+		let initialMessage = await message.channel.send(`Pong.`)
+		const diff = Number(process.hrtime.bigint() - time) * 1e-6;
+		initialMessage.edit(`Pong.\nAPI latency: ${apiLatency} ms\nMessage latency: ${diff.toFixed(0)} ms.`);
 	}
 };
