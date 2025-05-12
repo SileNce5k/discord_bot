be8d9660ba7d2a75f375f88765983d7f7ff5c5a7
SileNce5k <ozzynexus@gmail.com>
2025-05-12 20:11:34 +0200

    Fix permissions checking not working if false

--- commands/misc/prefix.js
+++ commands/misc/prefix.js
@@ -1,10 +1,11 @@
 const setServerPrefix = require("../../util/setServerPrefix");
+const { PermissionsBitField } = require('discord.js');
 
 module.exports = {
 	name: 'prefix',
 	description: 'Change the prefix of the bot in this server.',
 	execute({ message, client, args, prefix }) {
-		if (!message.member.permissions.has('MANAGE_GUILD')) {
+		if (!message.member.permissions.has(PermissionsBitField.Flags.ManageGuild)) {
 			message.channel.send("You do not have sufficient permissions(MANAGE_GUILD) to change the prefix of this server.")
 			return;
 		}
