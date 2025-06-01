15749b1a9f4dd6d3da1eb164bcdaa8bf6953b918
SileNce5k <ozzynexus@gmail.com>
2025-06-01 13:08:06 +0200

    Don't check for variables when updating presence

--- util/updatePresence.js
+++ util/updatePresence.js
@@ -4,7 +4,5 @@ module.exports = function (client) {
 	let presenceText = client.settings.get("presenceText")
 	let presenceType = client.settings.get("presenceType")
 
-	if(presenceText.includes("${guilds}") || presenceText.includes("${prefix}") || presenceText.includes("${uptime}")) {
-		setPresence({presenceText: presenceText, presenceType: presenceType, client: client});	
-	}
+	setPresence({presenceText: presenceText, presenceType: presenceType, client: client});	
 }
