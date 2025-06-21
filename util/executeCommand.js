08b52216a959739f7d1e1e5c9c6e4cf83cfac02e
SileNce5k <ozzynexus@gmail.com>
2025-06-21 17:50:29 +0200

    Compare with strict equality

--- util/executeCommand.js
+++ util/executeCommand.js
@@ -4,7 +4,7 @@ module.exports = function(command, commandArgs, verbose=false) {
     console.log("Executing:", command, commandArgs.join(" "));
     try {
         const output = execFileSync(command, commandArgs, {encoding: 'utf8'})
-        if (output.length != 0 && verbose)
+        if (output.length !== 0 && verbose)
             console.log(output)
     } catch (error) {
         console.error(`Error executing ${command} command:`, error);
