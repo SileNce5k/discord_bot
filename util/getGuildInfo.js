bbaf2205ade0fb76e2f82b879c99c48e8c4aab5b
SileNce5k <ozzynexus@gmail.com>
2025-06-24 02:28:24 +0200

    Add unique members to getGuildInfo

--- util/getGuildInfo.js
+++ util/getGuildInfo.js
@@ -1,9 +1,16 @@
 module.exports = function(client){
 	let guildCount = 0;
 	let totalMembers = 0;
+	let uniqueMembers = new Map();
 		client.guilds.cache.each(guild => {
 			guildCount++
 			totalMembers += guild.memberCount;
+			guild.members.cache.each(member => {
+				if(!uniqueMembers.get(member.id)){
+					uniqueMembers.set(member.id, true);
+				}
+			})
 		});
-	return {guildCount: guildCount, totalMembers: totalMembers};
+	let uniqueMemberCount = uniqueMembers.size;
+	return {guildCount, totalMembers, uniqueMemberCount};
 }
