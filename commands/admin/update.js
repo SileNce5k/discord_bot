65634641427d9e68946fdecb22a01fd803557680
SileNce5k <ozzynexus@gmail.com>
2025-11-26 14:43:06 +0100

    Separate updating to a separate function

--- commands/admin/update.js
+++ commands/admin/update.js
@@ -1,47 +1,12 @@
-const calculateReloaded = require("../../util/calculateReloaded");
-const executeCommand = require("../../util/executeCommand");
-const reloadCommands = require("../../util/reloadCommands");
+const update = require('../../util/update')
 
 module.exports = {
 	name: 'update',
 	description: 'pull changes from remote and reload commands with git',
 	admin: true,
 	execute({message, client}) { 
-		let cmd = "git pull --ff-only";
-		const exec = require("child_process").exec;
-		exec(cmd, (err, stdout, stderr) => {
-			process.stdout.write(stdout);
-			if(stdout.startsWith("Already up to date.")){
-				message.channel.send("Already up to date.\nNo updating needed.")
-			}else{
-				let beforeSize = client.commands.size;
-				reloadCommands(client)
-				let sendText = `${stdout}\nBot updated, and\n${calculateReloaded(beforeSize, client)}`
-				if(stdout.includes("server.js") || stdout.includes("server/")){
-					sendText = sendText + "\nServer.js OR a file the server/ directory has been updated.\nThis requires the bot to be restarted."
-				}
-				let regex = /([^\s]+)\.\.([^\s]+)/
-				let commits = stdout.match(regex)[0]
-				cmd = `git log --oneline ${commits}`;
-				exec(cmd, (err, stdout, stderr) =>{
-					process.stdout.write(stdout)
-					let commitCount = stdout.split(/\r\n|\r|\n/).length - 1
-					sendText = `${sendText}\n\nLatest commits (${commitCount}):\n${stdout}`
-					if(sendText.length >= 2000){
-						sendText = sendText.slice(1955)
-						sendText = `${sendText}\n... Message is too long to show everything`
-					}
-					message.channel.send(sendText)
-					const githash = executeCommand(`git`, ["rev-parse", "--short", "HEAD"]);
-					client.githash = githash.error ? "N/A" : githash.output;
-					if (err) console.log(stderr)
-				})
-			}
-			if (err) {
-				message.channel.send("Something went wrong...");
-				console.log(stderr);
-			}
-		});
+		let sendText = update(client);
+		message.channel.send(sendText);
 		
 	}
 };
---
+++ util/update.js
@@ -0,0 +1,32 @@
+const calculateReloaded = require("./calculateReloaded");
+const executeCommand = require("./executeCommand");
+const reloadCommands = require("./reloadCommands");
+
+module.exports = function(client){
+    let text = "";
+    let updateInfo = executeCommand("git", ["pull", "--ff-only"]);
+    if(updateInfo.error) {
+        console.log(stderr);
+        return "Something went wrong..."
+    }else {
+        const githash = executeCommand(`git`, ["rev-parse", "--short", "HEAD"]);
+        client.githash = githash.error ? "N/A" : githash.output;
+        let beforeSize = client.commands.size;
+        reloadCommands(client);
+        let reloadedText = calculateReloaded(beforeSize, client);
+        text = `${updateInfo.output}\nBot updated, and\n${reloadedText}`
+        if(updateInfo.output.includes("server.js") || updateInfo.output.includes("server/")){
+            text = text + "\nServer.js OR a file the server/ directory has been updated.\nThis requires the bot to be restarted."
+        }
+        const regex = /([^\s]+)\.\.([^\s]+)/
+        let commits = updateInfo.output.match(regex)[0]
+        const gitLog = executeCommand("git", ["log", "--oneline", commits]);
+        let commitCount = gitLog.output.split(/\r\n|\r|\n/).length - 1
+        text = `${text}\n\nLatest commits (${commitCount}):\n${gitLog.output}`;
+        if(text.length >= 2000) {
+            text = text.slice(1955);
+            text = `${text}\n... Message is too long to show everything`
+        }
+        return text;
+    }
+}
