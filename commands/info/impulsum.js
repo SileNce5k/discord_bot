2062a4ec5526f82c2ba6e4f57b8c96cc5001f877
SileNce5k <ozzynexus@gmail.com>
2026-09-05 07:39:44 +0200

    Add impulsum status command

---
+++ commands/info/impulsum.js
@@ -0,0 +1,16 @@
+const impulsumStatus = require('../../util/impulsumStatus')
+
+module.exports = {
+    name: "impulsum",
+    description: "Check how many people are online on impulsum servers",
+    async execute({message}) {
+       
+        let msg = await impulsumStatus(location).catch((err) => {
+            console.log(err);
+            if(msg === "") msg = "An error occured while trying to get the status of the impulsum servers.\nCheck server logs for more information."
+        });
+
+        message.channel.send(msg);
+    }
+
+}
